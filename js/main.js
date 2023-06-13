90996e727a5c283d59fffece648fee91176454e9
Matheus Augusto <129510488+maaat-aug@users.noreply.github.com>
2023-06-13 10:42:07 -0300

    Adjustments 🏡
    Adjustments🏡

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $("#navbar a, .btn").on("click", function (event) {
 // Sticky menu background
 window.addEventListener("scroll", function () {
     if (window.scrollY > 150) {
-        document.querySelector("#navbar").style.opacity = 0.9;
+        document.querySelector("#navbar").style.opacity = 0.8;
     } else {
         document.querySelector("#navbar").style.opacity = 1;
     }
